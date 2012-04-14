01776160c0a20902b4ed05625210cdc74a77d345
kevinalle <kevinalle@34645c33-9c57-f8fe-5b1b-bba07d7071d0>
2012-04-14 21:29:44 +0000

    anda para una instalacion limpia de ubuntu de 64bits

--- trunk/src/expofuse/asm.asm
+++ trunk/src/expofuse/asm.asm
@@ -1,46 +1,46 @@
 %define ARG1 [ebp+8]
 
 %macro Entrar 0
-	push ebp
-	mov ebp,esp
-	sub esp, 4
-	push esi
-	push edi
-	push ebx
+  push rbp
+  mov ebp,esp
+  sub esp, 4
+  push rsi
+  push rdi
+  push rbx
 %endmacro
 
 %macro Salir 0
-	pop ebx
-	pop edi
-	pop esi
-	add esp, 4
-	pop ebp
-	ret
+  pop rbx
+  pop rdi
+  pop rsi
+  add esp, 4
+  pop rbp
+  ret
 %endmacro
 
 section .data
-	C255: dd 255		; constante 255
+  C255: dd 255      ; constante 255
 section .text
 
-	; void asmInvertir(Matrix* A);
-	global asmInvertir
+  ; void asmInvertir(Matrix* A);
+  global asmInvertir
 
 asmInvertir:
-	Entrar					; Convencion C
-	mov esi,ARG1			; Cargo en es1 el 1er arg de la funcion
-	mov eax,[esi]			; eax= A->rows
-	mov ecx,[esi+4]		; ecx= A->cols
-	mul ecx					; eax=eax*ecx (cant de celdas)
-	mov ecx,eax				; ecx=cant celdas
-	mov edi,[esi+8]		; edi= *(A->data)
-	finit
-	.loop:
-		;fild dword [C255]
-		fld1
-		fld qword [edi]
-		fsub
-		fst qword [edi]
-		ffree
-		lea edi,[edi+8]
-		loop .loop
-	Salir
+  Entrar            ; Convencion C
+  mov esi,ARG1      ; Cargo en es1 el 1er arg de la funcion
+  mov eax,[esi]     ; eax= A->rows
+  mov ecx,[esi+4]   ; ecx= A->cols
+  mul ecx           ; eax=eax*ecx (cant de celdas)
+  mov ecx,eax       ; ecx=cant celdas
+  mov edi,[esi+8]   ; edi= *(A->data)
+  finit
+  .loop:
+    ;fild dword [C255]
+    fld1
+    fld qword [edi]
+    fsub
+    fst qword [edi]
+    ffree
+    lea edi,[edi+8]
+    loop .loop
+  Salir
