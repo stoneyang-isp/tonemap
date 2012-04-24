0fe8c7ca9c9d51c53d4f751221c4845eb181f6c9
kevinalle <kevinalle@34645c33-9c57-f8fe-5b1b-bba07d7071d0>
2012-04-24 21:08:35 +0000

    Matrix substract SSE :)

--- src/expofuse/Matrix.asm
+++ src/expofuse/Matrix.asm
@@ -30,7 +30,7 @@ section .text
   ; Matrix* _asmSubstract(const double* A, const double* B, double* C, int rows, int cols);
   global _asmSubstract
 
-_asmSubstract:
+_asmSubstractFPU:
   Entrar            ; Convencion C
   mov r10, ARG3     ; ARG3 se va a pisar con el mul
   mov rax, ARG4     ; rax = rows
@@ -47,4 +47,30 @@ _asmSubstract:
     lea ARG2, [ARG2+8]
     lea r10, [r10+8]
     loop .loop
+  Salir
+
+_asmSubstract:
+  Entrar               ; Convencion C
+  mov r10, ARG3        ; ARG3 se va a pisar con el mul
+  mov rax, ARG4        ; rax = rows
+  mul ARG5             ; rax = rows*cols
+  mov rcx, rax         ; rcx = total celdas
+  shr rcx, 1           ; rcx = total celdas / 2
+  jnc .loop            ; if (celdas impares) {
+    movsd xmm1, [ARG1] ;   C[0] = A[0] - B[0]
+    movsd xmm2, [ARG2]
+    subsd xmm1, xmm2
+    movsd [r10], xmm1
+    lea ARG1, [ARG1+8]
+    lea ARG2, [ARG2+8] ;   i++
+    lea r10, [r10+8]   ; }
+  .loop:               ; while (RCX) {
+    movupd xmm1, [ARG1];   C[i] = A[i] - B[i]; C[i+1] = A[i+1] - B[i+1]
+    movupd xmm2, [ARG2]
+    subpd xmm1, xmm2
+    movupd [r10], xmm1
+    lea ARG1, [ARG1+16]
+    lea ARG2, [ARG2+16]
+    lea r10, [r10+16]  ;   i+=2; RCX-=2
+    loop .loop         ; }
   Salir
